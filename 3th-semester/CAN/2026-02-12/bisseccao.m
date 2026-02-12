3274603d468f610cd8c6e92ceee7eeb0180cba6d
vvguerravv <victor.eg@aluno.ifsc.edu.br>
2026-02-12 11:23:11 -0300

    CAN

--- 3th-semester/CAN/2026-02-12/bisseccao.m
+++ 3th-semester/CAN/2026-02-12/bisseccao.m
@@ -1,7 +1,8 @@
 f= @(x) x^2 + 2*x - 2;
-a = 0;
+a = -2;
 b = -3;
 error = 1;
+
 while (error > 10^-4);
     
     x = (a+b) / 2;
@@ -15,9 +16,19 @@ while (error > 10^-4);
     end
     
     error = abs(b - a);
+    
+    x = (a+b) / 2;
+
+    if(f(a) > 0 && f(b) < 0 && f(x) < 0)
+        b = x;
+    end
+
+    if(f(a) > 0 && f(b) < 0 && f(x) > 0)
+        a=x;
+    end
+    
 end
 
-x
 
 
 
